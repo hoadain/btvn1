d216c815c1ab0ea0dfa50fcf0fc0324ef69d8f19
Your Name <you@example.com>
2024-09-08 09:34:09 +0700

    Add project files.

---
+++ btvn1/Program.cs
@@ -0,0 +1,17 @@
+﻿// nguyễn đình hòa
+// quản lí 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btvn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
