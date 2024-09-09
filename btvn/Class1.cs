0d72a42d8ab73cbd1176f04e38daba56a06c148d
Your Name <you@example.com>
2024-09-09 21:36:42 +0700

    tinh toan

---
+++ btvn/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using btvn;
+
+namespace btvn
+{//
+    public class Class1
+
+    {
+        public int SoTienGui { get; set; }
+        public double LaiSuat { get; set; } // Phần trăm
+        public int SoNamGui { get; set; }
+        public double LaiSuatTienGui { get; private set; }
+        //tinh taon
+        public void TinhLaiSuat()
+        {
+            LaiSuatTienGui = SoTienGui * (LaiSuat / 100) * SoNamGui;
+        }
+
+        public void InKetQua()
+        {
+            Console.WriteLine("Số tiền lãi nhận được sau {0} năm là: {1} VNĐ", SoNamGui, LaiSuatTienGui);
+        }
+    }
+}
--- btvn1/Program.cs
+++ btvn1/Program.cs
@@ -1,5 +1,5 @@
-﻿// nguyễn đình hòa
-// tính lãi xuất tiền gửi 
+﻿// Họ và tên: nguyễn đình hòa
+// Bắt đầu làm bài toán: tính lãi xuất tiền gửi 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +12,16 @@ namespace btvn1
     {
         static void Main(string[] args)
         {
-            // Nhập số tiền gửi
+        
             Console.Write("Nhập số tiền gửi (VNĐ): ");
             double soTienGui = Convert.ToDouble(Console.ReadLine());
-            // Nhập lãi suất hàng năm
+           
             Console.Write("Nhập lãi suất (%): ");
             double laiSuat = Convert.ToDouble(Console.ReadLine());
-            // Nhập số năm gửi
+            
             Console.Write("Nhập số năm gửi: ");
             double soNamGui = Convert.ToDouble(Console.ReadLine());
-            // Tính lãi suất
+
             double laiSuatTienGui = soTienGui * (laiSuat / 100) * soNamGui;
             Console.WriteLine("Số tiền lãi nhận được sau {0} năm là: {1} VNĐ", soNamGui, laiSuatTienGui);
             Console.ReadKey();
