d8fdda7bfaa4e2b5da03187e35de864f973063f1
Your Name <you@example.com>
2024-09-10 19:12:47 +0700

    vẫn còn nhiều thiếu sót về code, tìm tòi hồi lâu mãi cũng ra thôi

---
+++ WindowsFormstinhlais/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormstinhlais
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double tienGoc = double.Parse(textBox1.Text);      
+                double laiSuatNam = double.Parse(textBox2.Text);   
+                double soNam = double.Parse(textBox3.Text);        
+
+                Calculator calculator = new Calculator();
+
+                double tienLai = calculator.TinhTienLai(tienGoc, laiSuatNam, soNam);
+                double tongTien = calculator.TinhTongTien(tienGoc, tienLai);
+                lblTienLai.Text = $"Số tiền lãi sau {soNam} năm: {tienLai:N0} VND";
+                lblTongTien.Text = $"Tổng số tiền sau {soNam} năm: {tongTien:N0} VND";
+            }
+            catch (Exception ex)
+            {
+                // Hiển thị thông báo lỗi khi nhập sai
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    // Lớp Calculator để tính toán
+    public class Calculator
+    {
+        // Phương thức tính tiền lãi
+        public double TinhTienLai(double tienGoc, double laiSuatNam, double soNam)
+        {
+            return tienGoc * laiSuatNam / 100 * soNam;
+        }
+
+        // Phương thức tính tổng tiền sau khi có lãi
+        public double TinhTongTien(double tienGoc, double tienLai)
+        {
+            return tienGoc + tienLai;
+        }
+    }
+}
+    
